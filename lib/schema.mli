71d55c0fc14db5482b608ae1b08fc229fbaf75cb
nogw <gabrielnogueiraoliveira@gmail.com>
2022-06-23 13:20:45 -0300

    refactor a few things

--- lib/schema.mli
+++ lib/schema.mli
@@ -2,8 +2,6 @@ module Schema : sig
   type t = Schema of (string * Validate.Validate.options) list
 
   val value : t -> (string * Validate.Validate.options) list
-
-  val is_valid : t -> t
 end
 
 val validate_json : (string * Yojson.Safe.t) list -> (string * Validate.Validate.options) list -> (Yojson.Safe.t, string) result

7aa072ad9222db81eb3c1d1004efff88a89f14f3
nogw <gabrielnogueiraoliveira@gmail.com>
2022-06-23 13:20:25 -0300

    refactor a few things

--- lib/schema.ml
+++ lib/schema.ml
@@ -1,57 +1,51 @@
 open Errors
 open Validate
 
-module Schema = struct 
+module Schema = struct
   type t = Schema of (string * Validate.options) list
 
-  let value = function | Schema v -> v 
-
-  let is_valid s =
-    if s |> value |> List.length > 0 then s
-    else raise (Invalid_argument "Invalid Schema")
+  let value = function Schema v -> v
 end
 
-open Schema
-
 let rec validate_field field (value : Yojson.Safe.t) validator =
-  let aux rest f =
-    match f with Error e -> Error e | Ok _ -> validate_field field value rest
+  let callback rest f =
+    match f with 
+    | Error e -> Error e 
+    | Ok _ -> validate_field field value rest
   in
   match validator with
-  | Validate.String ls -> (
-      match ls with
-      | v :: rest -> StringValid.validate v field value |> aux (String rest)
+  | Validate.String vs -> (
+      match vs with
+      | v :: rest -> StringValid.validate v field value |> callback (String rest)
       | [] -> Ok field)
-  | Validate.Number ls -> (
-      match ls with
-      | v :: rest -> NumberValid.validate v field value |> aux (Number rest)
+  | Validate.Number vs -> (
+      match vs with
+      | v :: rest -> NumberValid.validate v field value |> callback (Number rest)
       | [] -> Ok field)
-  | Validate.List ls -> (
-      match ls with
-      | v :: rest -> ListValid.validate v field value |> aux (List rest)
+  | Validate.List vs -> (
+      match vs with
+      | v :: rest -> ListValid.validate v field value |> callback (List rest)
       | [] -> Ok field)
-
-let is_required validators =
-  match validators with
-  | Validate.String ls -> List.mem StringValid.Required ls
-  | Validate.Number ls -> List.mem NumberValid.Required ls
-  | Validate.List ls -> List.mem ListValid.Required ls
 
 let rec validate_json json validators =
   match validators with
   | (field, validator) :: rest -> (
       match List.assoc_opt field json with
       | None ->
-          if is_required validator then
-            Error (IsRequired field |> errors_to_string)
-          else validate_json json rest
+          if Validate.is_required validator 
+            then Error (IsRequired field |> errors_to_string)
+            else validate_json json rest
       | Some v -> (
           match validate_field field v validator with
           | Ok _ -> validate_json json rest
           | Error e -> Error (e |> errors_to_string)))
   | [] -> Ok (`Assoc json)
 
-let validate validate json =
-  match (json, validate) with
-  | `Assoc j, Schema v -> validate_json j v
-  | _ -> failwith "Invalid props"
+let validate schema json =
+  let open Schema in 
+
+  let schema = schema |> value in
+  
+  let json = json |> Yojson.Safe.Util.to_assoc in 
+  
+  validate_json json schema
